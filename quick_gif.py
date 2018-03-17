13e43929603c28551c1d2f7cc73558663e86edce
Lindsey B <lindseyb@github.com>
2018-03-16 20:09:06 -0400

    take an out path for output

--- quick_gif.py
+++ quick_gif.py
@@ -69,6 +69,7 @@ def makeGif(video, start, end, string, output):
 def main():
   stream = file('files.yml', 'r')
   data = yaml.load(stream)
+  out_path = data["out_path"]
 
   for file_data in data["files"]:
     video_file_path = file_data["video"]
@@ -81,7 +82,7 @@ def main():
     for sub in subs:
       start = str(sub.start)
       end = str(sub.end - sub.start)
-      makeGif(video_file_path, start, end, sub.text, slugify(sub.text) + ".gif")
+      makeGif(video_file_path, start, end, sub.text, os.path.join(outpath, slugify(sub.text) + ".gif")
 
   shutil.rmtree(directory)
 
